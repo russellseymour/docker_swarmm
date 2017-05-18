2daac7a4203ef5e1f1f10560589d82902be4f9dd
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2017-05-18 20:12:13 +0100

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name 'docker-swarmm'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures docker-swarmm'
+long_description 'Installs/Configures docker-swarmm'
+version '0.1.0'
+
+chef_version '>= 12'
+
+issues_url 'https://github.com/company/cookbook/issues'
+source_url 'https://github.com/company/cookbook'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: docker-swarmm
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
