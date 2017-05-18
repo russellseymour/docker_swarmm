32ffa2ad31cec8139c4abbd8e65aa4b88e720c80
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2017-05-18 20:12:15 +0100

    Add generated delivery build cookbook

---
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: build-cookbook
+# Recipe:: publish
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+include_recipe 'delivery-truck::publish'
---
+++ .delivery/build-cookbook/recipes/syntax.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: build-cookbook
+# Recipe:: syntax
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+include_recipe 'delivery-truck::syntax'
