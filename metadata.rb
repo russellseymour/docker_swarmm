4bfbf271ba230f28f837ed173f652d29ee8291af
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2017-06-02 17:10:25 +0100

    Updated metadata.rb file
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,16 @@
 
 name "docker_swarmm"
-maintainer "The Authors"
-maintainer_email "you@example.com"
-license "all_rights"
+maintainer       "Russell Seymour"
+maintainer_email "russell.seymour@turtlesystems.co.uk"
+license "All rights reserved"
 description "Installs/Configures docker-swarmm"
 long_description "Installs/Configures docker-swarmm"
 version "0.1.0"
 
-chef_version ">= 12"
+chef_version ">= 12.5" if respond_to?(:chef_version)
 
-issues_url "https://github.com/company/cookbook/issues"
-source_url "https://github.com/company/cookbook"
+issues_url "" if respond_to?(:issues_url)
+source_url "" if respond_to?(:source_url)
 
 # Set the platforms that this will run on
 supports "ubuntu"
