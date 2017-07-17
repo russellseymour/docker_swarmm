c82a84813eabe011234600d560c6fcc057421df7
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2017-07-17 15:22:12 +0100

    Added ability for nodes to be added as workers to the swarm cluster
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

---
+++ libraries/docker_swarm_worker.rb
@@ -0,0 +1,24 @@
+require "mixlib/shellout"
+
+module DockerSwarmCookbook
+  # Docker Master class to configure the manager node
+  class DockerSwarmMaster < DockerSwarmBase
+    resource_name :docker_swarm_worker
+
+    default_action :run
+
+    # Get the properties to run with
+    property :manager_address, String, name_property: true
+    property :worker_token, String, required: true
+    property :manager_port, Integer, default: 2377
+
+    action :run do
+      # Attempt to join the existing cluster
+      execute "join swarm" do
+        command format("docker swarm join --token %s %s:%s", worker_token, manager_address, manager_port)
+
+        not_if 'docker info 2>/dev/null | grep "Swarm: active"'
+      end
+    end
+  end
+end
